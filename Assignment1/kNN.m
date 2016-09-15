6b8d307f54f68fd39d1cdedacd2b308b20629754
Casey O'Kane <caseyokane@Caseys-MacBook-Pro.local>
2016-09-15 11:42:48 -0400

    kNN seems to be working now. Still need more involved testing and to start on decision trees

--- Assignment1/kNN.m
+++ Assignment1/kNN.m
@@ -4,13 +4,39 @@
 %kNN Implementation
 
 
-function kNNLabel = kNN(testPoint, k, trainingPoints, trainingLabels)
+function kNNLabel = kNN(testPoint, k, trainPoints, trainLabels)
 
-    %Using feature x instance table 
-    %Calculate Euclidean distance using L2_distance.m
-    %determine distance between test point and k training points
-    %sort to find smallest distance
-    %return label of shortest 
+
+%Generate Distances
+    %Initialize distance matrix
+    numTrainInst = numel(trainPoints(:,1));
+    distMat = zeros([numTrainInst 2]); distMat(:,2) = 1:numTrainInst;
     
+    %Initialize k value matrix to store k values and indices 
+    kNNMat = zeros([k 2]);
+    %Initialize array of labels to store the labels associated with kNN
+    kNNLabel = zeros([1 k]);
+    
+    
+    %iterate through each row element in a column tPoints(:, ctr)
+    for ctr = 1:numTrainInst
+        %Calculate Euclidean distance using L2_distance.m with transpose
+        kDist = L2_distance(testPoint', trainPoints(ctr,:)'); 
+        %store result in distMat column, with index in 2nd column
+        distMat(ctr,1) = kDist;
+    end
 
+%Find K closest values 
+    %While there are still some k values left
+    for kCtr = 1:k
+        %Find the minimum distance and it's related index
+        [minK, minKind] = min(distMat(:,1));
+        %Remove row associated at that index to find the the new minimum
+        distMat(minKind,:) = [];
+        %Place minK and it's index in kNN array with index 
+        kNNMat(kCtr,1) = minK; kNNMat(kCtr,2) = minKind;
+        %use minKind to find labels associated with kvalues
+        kNNLabel(kCtr) = trainLabels(minKind,1);
+    end
+    
 end
