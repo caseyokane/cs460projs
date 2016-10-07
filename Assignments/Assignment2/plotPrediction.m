34df74a0db98ac69b19b3d4273a62e7cc10bce70
Casey O'Kane <casey.okane@uky.edu>
2016-10-07 17:23:06 -0400

    Final commit, post submission

--- Assignments/Assignment2/plotPrediction.m
+++ Assignments/Assignment2/plotPrediction.m
@@ -5,22 +5,17 @@ function plotPrediction(numIter, trFeats, trLabels, teFeats, teLabels, mseVal, p
 
     currOrder = abs(1 - numIter);
     currTitle = strcat(num2str(currOrder), ' Order Linear Regression using ', method, ' prediction');
-    %line space for linear regression prediction 
     
+    %line space for linear regression prediction 
     trainLspace = linspace(min(trFeats), max(trFeats), size(trFeats,1));
-    %trainLspace = linspace(-1.5, 1.5, 25);
     
     %Plot for the  Normal Method
     %using the results from fitLinRegNormal, form a polynomial to plot 
     figure(); clf;
-    %scatter(testLspace, hw2Train(:,2),25,'green')
-    %scatter(trainLspace, hw2Train(:,2),25,'green')
     plot(trFeats,trLabels, 'og')
     hold on;
-    %scatter(xLspace, hw2Test(:,1),25,'red')
-    %scatter(testLspace, hw2Test(:,2),25,'red') 
     plot(teFeats, teLabels, 'or')
-    %plot(trainLspace, predLabels)
+    %plot(trFeats, predLabels)
     plot(trainLspace, polyval(predLabels,trainLspace))
     title(currTitle)
     xlabel('Feature values')
