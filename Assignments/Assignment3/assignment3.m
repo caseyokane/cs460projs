1ee0fe6e4376de7c979effcbb37158dd172d4174
caseyokane <casey.okane@uky.edu>
2016-11-05 00:01:13 -0400

    Staging commit

--- Assignments/Assignment3/assignment3.m
+++ Assignments/Assignment3/assignment3.m
@@ -7,23 +7,32 @@
 %Import data sets
 data = csvread('data/synthetic-1.csv');
 
+%compute cost of logisitc regression
+hTheta = logRegHyp(data);
+
+
 %Implement logistic regression hypothesis function using sigmoid function
-hTheta = logRegHyp(data(1:2,:));
+%hTheta = logRegHyp(data(1:2,:));
 
 %Implement grid sampling the logistic regression function for a range of
 %parameters and then show result
-figure(2); clf;
 Xmin = min(data(:,1)); Xmax = max(data(:,1));
 Ymin = min(data(:,2)); Ymax = max(data(:,2));
+
+%Define the function for logistic regression plotting
+f = @(vals) 1./(1+exp(-[0, 1, -.5,.5]*vals));
+
 [XX,YY] = meshgrid(linspace(Xmin, Xmax,200), linspace(Ymin, Ymax,200));
 
 hTheta = reshape(hTheta,size(XX));
 
+figure(1); clf;
 imagesc(hTheta, 'Xdata', [Xmin,Xmax], 'Ydata', [Ymin,Ymax], [0 1])
 hold on
-scatter(data(:,1), data(:,2), 40, data(:,3), 'filled');
+scatter(data(:,1), data(:,2), 30, data(:,3), 'filled');
 contour(XX,YY,hTheta,[.5 .5], 'k')
-set(h, 'MarkerEdgeColor', 'w')
+set(h, 'MarkerEdgeColor', 'r')
 hold off
 axis equal
-title(sprintf('%s %s',1,1)) 
+%Include variable title
+%title(sprintf('%s %s',1,1)) 
--- Assignments/Assignment3/logRegHyp.m
+++ Assignments/Assignment3/logRegHyp.m
@@ -1,8 +1,21 @@
-function  hTheta = logRegHyp( smplPts )
+function  hTheta = logRegHyp( data )
 
     %Using the sample points, determine Theta.' * x
-    %Options is needed here, the sample points are the features space
-    hTheta = sigmoid(smplPts * Theta.');
+    %Rename data set with friendlier variables
+    feats = data(:,1:2); labs = data(:,3);
+
+    %TODO: CLEAN BELOW
+    %Get dimesions of the data set 
+    [rows, cols] = size(feats);
+    %Set up data matrix to include bias term
+    X = [ones(rows,1), feats];
+    
+    %Initialize parameter values equal to 0 initially
+        %TODO: Maybe change cols +1 to size()
+    hTheta = zeros(cols+1, 1);
+    
+    %Compute the cost value using the sigmoid function
+    hTheta = sigmoid(X * hTheta);
 
 end
 
