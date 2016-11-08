11fcbc5a63f4af046d03313801886838cf4bef83
Casey O'Kane <casey.okane@uky.edu>
2016-11-08 17:05:20 -0500

    Final implementation

--- Puzzlers/puzzler3.m
+++ Puzzlers/puzzler3.m
@@ -2,3 +2,11 @@
 % Casey O'Kane
 % 10/8/16
 
+%First, initialize variables as provided in the prompt
+H = [2,1;1,2];
+f = [1,1]';
+A = [1,0];
+b = -3;
+
+%Find the minimum value of x for (1/2x' * H * x)  + (f' * x)
+x = quadprog(H,f,A,b)'
