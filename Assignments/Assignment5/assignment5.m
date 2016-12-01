4787ea24e2707737d5c2207183e0f7b9e727ebee
caseyokane <casey.okane@uky.edu>
2016-11-30 23:54:30 -0500

    Initial commit for Assignment 5

---
+++ Assignments/Assignment5/assignment5.m
@@ -0,0 +1,13 @@
+%CS460 - Machine Learning 
+%Assignment 5 - Leaf Classification 
+%Casey O'Kane 
+%Random Forest Classifier Version
+
+%import dataset
+trainData = csvread('/data/train.csv');
+testData = csvread('/data/test.csv');
+
+%implement random forest
+
+
+%generate the correct csv file using the test labels
